1c6d82c4d72fe39ed48fe032ab5ed73db09844ef
Fatal1990 <96417741+Fatal1990@users.noreply.github.com>
2022-06-27 21:52:06 +0300

    lebedev_s

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,112 @@
 <template>
-  <CategoryCatalogComponent></CategoryCatalogComponent>
+  <header></header>
+  <!-- <section class="banner">
+    <div class="banner__img-w">
+      <picture>
+        <source srcset="https://logicpower.ua/images/catalog/Продукция-1.png" media="(max-width: 1280px)">
+        <img class="banner__img" src="https://logicpower.ua/storage/1908309/Banner_LP_15_06.png" alt="">
+      </picture>
+    </div>
+    <div class="banner__content">
+      <div class="banner__content-column">
+        <h3 class="banner__title">Заголовок</h3>
+        <p class="banner__text">текст</p>
+        <input class="banner__input" type="text" value="123" />
+      </div>
+    </div>
+  </section> -->
+  <main class="site-main">
+    <!-- <CategoryCatalogComponent></CategoryCatalogComponent> -->
+    <!-- <AboutCompanyComponent></AboutCompanyComponent> -->
+    <!-- <PrivacyPolicyComponent></PrivacyPolicyComponent> -->
+    <!-- <WarrantyComponent></WarrantyComponent> -->
+    <PublicOfferComponent></PublicOfferComponent>
+  </main>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import CategoryCatalogComponent from "./components/CategoryCatalog.vue";
+import AboutCompanyComponent from "./components/AboutCompany.vue";
+import PrivacyPolicyComponent from "./components/PrivacyPolicy.vue";
+import WarrantyComponent from "./components/Warranty.vue";
+import PublicOfferComponent from "./components/PublicOffer.vue";
 
 @Options({
-  components: { CategoryCatalogComponent },
+  components: {
+    CategoryCatalogComponent,
+    AboutCompanyComponent,
+    PrivacyPolicyComponent,
+    WarrantyComponent,
+    PublicOfferComponent,
+  },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
+@import "assets/scss/style.scss";
 #app {
   display: contents;
 }
 </style>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.banner {
+  position: relative;
+  height: 560px;
+  width: 100%;
+
+  background-color: black;
+
+  &__img-w {
+    height: 100%;
+    width: 100%;
+  }
+
+  &__img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+
+  &__content {
+    @include absoluteGrow();
+    display: flex;
+    justify-content: center;
+
+    background: linear-gradient(
+      180deg,
+      rgba(138, 138, 138, 0) 0%,
+      rgba(54, 54, 54, 0.37) 56.63%,
+      rgba(54, 54, 54, 0.83) 81.98%
+    );
+
+    z-index: 2;
+  }
+
+  &__content-column {
+    width: 100%;
+    max-width: 1500px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    gap: 24px;
+
+    padding: 16px 0 64px;
+
+    color: white;
+  }
+
+  &__title {
+    @include font30;
+  }
+
+  &__text {
+  }
+
+  &__input {
+  }
+}
+</style>
--- src/components/CategoryCatalog.vue
+++ src/components/CategoryCatalog.vue
@@ -1,11 +1,184 @@
 <template>
-  <div class="category-catalog">
-    <div class="category-card">
-      <img src="https://logicfox.info/foto/14292/14292_1.png" alt="" />
-      <a href="#">перейти</a>
-      <h2>Лучшая категория в мире</h2>
+  <section class="category-catalog">
+    <h1 class="category-catalog__title">Продукция</h1>
+    <div class="category-catalog__w">
+      <div class="category-catalog__item category-card">
+        <a class="category-card__link" href="#">
+          <img class="category-card__img" src="https://logicpower.ua/storage/1903113/12258_1.jpg" alt=""/>
+        </a>
+        <div class="category-card__info">
+          <h3 class="category-card__title">Комплекты для резервного питания</h3>
+          <p class="category-card__text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequatur, similique! Animi magni accusantium nemo voluptatibus
+            voluptate laboriosam, molestiae vero nulla soluta exercitationem
+            quae delectus, quidem, adipisci aperiam blanditiis incidunt
+            distinctio.
+          </p>
+          <a class="category-card__link" href="#">перейти</a>
+        </div>
+      </div>
+      <div class="category-catalog__item category-card">
+        <a class="category-card__link" href="#">
+          <img class="category-card__img" src="https://logicpower.ua/storage/1903100/30_1.jpg" alt="" />
+        </a>
+        <div class="category-card__info">
+          <h3 class="category-card__title">Комплекты для резервного питания</h3>
+          <p class="category-card__text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequatur, similique! Animi magni accusantium nemo voluptatibus
+            voluptate laboriosam, molestiae vero nulla soluta exercitationem
+            quae delectus, quidem, adipisci aperiam blanditiis incidunt
+            distinctio.
+          </p>
+          <a class="category-card__link" href="#">перейти</a>
+        </div>
+      </div>
+      <div class="category-catalog__item category-card">
+        <a class="category-card__link" href="#">
+          <img class="category-card__img" src="https://logicfox.info/foto/14292/14292_1.png" alt="" />
+        </a>
+        <div class="category-card__info">
+          <h3 class="category-card__title">Комплекты для резервного питания</h3>
+          <p class="category-card__text">Lorem ipsum</p>
+          <a class="category-card__link" href="#">перейти</a>
+        </div>
+      </div>
+      <div class="category-catalog__item category-card">
+        <a class="category-card__link" href="#">
+          <img class="category-card__img" src="https://logicfox.info/foto/14292/14292_1.png" alt="" />
+        </a>
+        <div class="category-card__info">
+          <h3 class="category-card__title">Комплекты для резервного питания</h3>
+          <p class="category-card__text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequatur, similique!
+          </p>
+          <a class="category-card__link" href="#">перейти</a>
+        </div>
+      </div>
+      <div class="category-catalog__item category-card">
+        <a class="category-card__link" href="#">
+          <img class="category-card__img" src="https://logicfox.info/foto/14292/14292_1.png" alt="" />
+        </a>
+        <div class="category-card__info">
+          <h3 class="category-card__title">Комплекты для резервного питания</h3>
+          <p class="category-card__text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequatur, similique! Animi magni accusantium nemo voluptatibus
+            voluptate laboriosam, molestiae vero nulla soluta exercitationem
+            quae delectus, quidem, adipisci aperiam blanditiis incidunt
+            distinctio.
+          </p>
+          <a class="category-card__link" href="#">перейти</a>
+        </div>
+      </div>
+      <div class="category-catalog__item category-card">
+        <a class="category-card__link" href="#">
+          <img class="category-card__img" src="https://logicfox.info/foto/14292/14292_1.png" alt="" />
+        </a>
+        <div class="category-card__info">
+          <h3 class="category-card__title">Комплекты для резервного питания</h3>
+          <p class="category-card__text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequatur, similique! Animi magni accusantium nemo voluptatibus
+            voluptate laboriosam, molestiae vero nulla soluta exercitationem
+            quae delectus, quidem, adipisci aperiam blanditiis incidunt
+            distinctio.
+          </p>
+          <a class="category-card__link" href="#">перейти</a>
+        </div>
+      </div>
+      <div class="category-catalog__item category-card">
+        <a class="category-card__link" href="#">
+          <img class="category-card__img" src="https://logicfox.info/foto/14292/14292_1.png" alt="" />
+        </a>
+        <div class="category-card__info">
+          <h3 class="category-card__title">Комплекты для резервного питания</h3>
+          <p class="category-card__text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequatur, similique! Animi magni accusantium nemo voluptatibus
+            voluptate laboriosam, molestiae vero nulla soluta exercitationem
+            quae delectus, quidem, adipisci aperiam blanditiis incidunt
+            distinctio.
+          </p>
+          <a class="category-card__link" href="#">перейти</a>
+        </div>
+      </div>
+      <div class="category-catalog__item category-card">
+        <a class="category-card__link" href="#">
+          <img class="category-card__img" src="https://logicfox.info/foto/14292/14292_1.png" alt="" />
+        </a>
+        <div class="category-card__info">
+          <h3 class="category-card__title">Комплекты для резервного питания</h3>
+          <p class="category-card__text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Consequatur, similique! Animi magni accusantium nemo voluptatibus
+            voluptate laboriosam, molestiae vero nulla soluta exercitationem
+            quae delectus, quidem, adipisci aperiam blanditiis incidunt
+            distinctio.
+          </p>
+          <a class="category-card__link" href="#">перейти</a>
+        </div>
+      </div>
     </div>
-  </div>
+    <div class="category-purpose">
+      <div class="category-purpose__line"></div>
+      <div class="category-purpose__w">
+        <h2 class="category-purpose__title">цель компании logicpower</h2>
+        <div class="category-purpose__info">
+          <p class="category-purpose__info-text">
+            Цель компании LogicPower - поставлять украинскому потребителю товары
+            высокого качества по доступной цене! В разделах «Компьютерная
+            периферия» и «Комплектующие для ПК» Вы найдете все для корректной и
+            комфортной работы своего компьютера. В разделе «Электропитание» -
+            товары, которые обеспечат Ваше оборудование качественным питанием и
+            обезопасят его.
+          </p>
+          <ul class="category-purpose__info-list">
+            <li class="category-purpose__info-list-item">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </li>
+            <li class="category-purpose__info-list-item">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </li>
+            <li class="category-purpose__info-list-item">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </li>
+            <li class="category-purpose__info-list-item">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </li>
+            <li class="category-purpose__info-list-item">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            </li>
+          </ul>
+        </div>
+        <div class="category-purpose__goal">
+          <h4 class="category-purpose__goal-title">Цель компании LogicPower</h4>
+          <p class="category-purpose__goal-text">
+            Цель компании LogicPower - поставлять украинскому потребителю товары
+            высокого качества по доступной цене! В разделах «Компьютерная
+            периферия» и «Комплектующие для ПК» Вы найдете все для корректной и
+            комфортной работы своего компьютера. В разделе «Электропитание» -
+            товары, которые обеспечат Ваше оборудование качественным питанием и
+            обезопасят его.
+          </p>
+        </div>
+        <div class="category-purpose__goal">
+          <h4 class="category-purpose__goal-title">Цель компании LogicPower</h4>
+          <p class="category-purpose__goal-text">
+            Цель компании LogicPower - поставлять украинскому потребителю товары
+            высокого качества по доступной цене! В разделах «Компьютерная
+            периферия» и «Комплектующие для ПК» Вы найдете все для корректной и
+            комфортной работы своего компьютера. В разделе «Электропитание» -
+            товары, которые обеспечат Ваше оборудование качественным питанием и
+            обезопасят его.
+          </p>
+        </div>
+      </div>
+      <div class="category-purpose__line"></div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
@@ -17,43 +190,242 @@ export default class CategoryCatalogComponent extends Vue {}
 
 <style lang="scss">
 .category-catalog {
-  display: flex;
-  gap: 16px;
-  max-width: 900px;
-  margin: 64px 0;
-}
-
-.category-card {
-  position: relative;
-
-  height: 300px;
-  width: 200px;
-
-  display: flex;
-  flex-direction: column;
+  @extend %flex-column;
+  justify-content: center;
   align-items: center;
+  gap: 48px;
 
-  padding: 4px;
+  @extend %padding-wrp;
+  background-color: $color-bg-grey-white;
 
-  color: black;
-  @include fontMid();
+  @include mobile {
+    gap: 36px;
+  }
 
-  background-color: white;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  &__title {
+    @include fontUnify(42, 50, 500);
+    text-align: center;
+    text-transform: uppercase;
 
-  @include animAll();
-  z-index: 2;
+    margin-bottom: 46px;
 
-  &:hover {
-    img {
-      transform: scale(1.1) rotate(10deg);
+    @include mobile {
+      @include fontUnify(24, 34, 500);
+      letter-spacing: 0.02em;
+
+      margin-bottom: 0;
+    }
+  }
+  &__w {
+    --gap: 16px;
+    @extend %width-main;
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    gap: 24px Var(--gap);
+
+    @include mobile {
+      gap: Var(--gap);
     }
   }
 
-  img {
-    width: 200px;
-    height: 300px;
+  &__item {
+    @include set-cart-count-in-row(4);
+
+    @include smallScreen() {
+      @include set-cart-count-in-row(3);
+    }
+
+    @include bigMobile() {
+      @include set-cart-count-in-row(2);
+    }
+
+    @include mobile() {
+      @include set-cart-count-in-row(1);
+    }
+  }
+}
+
+.category-card {
+  // max-width: 363px;
+  // width: 100%;
+
+  @extend %flex-column;
+  align-items: center;
+  gap: 24px;
+
+  background-color: #ffffff;
+  box-shadow: 0px 3px 11px rgba(0, 0, 0, 0.2);
+
+  border-radius: 8px;
+
+  @include mobile {
+    // max-width: 343px;
+    gap: 16px;
+  }
+
+  &__img {
+    width: 100%;
+    height: 302px;
+
+    border-top-left-radius: inherit;
+    border-top-right-radius: inherit;
+
+    object-fit: cover;
     pointer-events: none;
+
+    @include mobile {
+      height: 285px;
+    }
+  }
+
+  &__info {
+    height: 100%;
+
+    @extend %flex-column;
+    align-items: center;
+    justify-content: space-between;
+    gap: 16px;
+
+    padding: 0 10px 20px;
+
+    @include mobile {
+      gap: 46px;
+
+      padding-bottom: 24px;
+    }
+  }
+  &__title {
+    @include fontUnify(20, 30);
+    text-transform: uppercase;
+    text-align: center;
+
+    @include mobile {
+      @include fontUnify(18, 24);
+      letter-spacing: 0.02em;
+    }
+  }
+  &__text {
+    @include fontUnify;
+    text-align: center;
+    letter-spacing: 0.02em;
+
+    color: #8a8a8a;
+
+    @include lineClamp(4);
+
+    @include mobile {
+      display: none;
+    }
+  }
+  &__link {
+    width: 100%;
+
+    @include fontUnify(18, 24);
+    text-transform: capitalize;
+    text-align: center;
+    letter-spacing: 0.02em;
+
+    color: $color-main;
+  }
+}
+
+.category-purpose {
+  width: 100%;
+
+  @extend %flex-column;
+  justify-content: center;
+  align-items: center;
+  gap: 48px;
+
+  &__w {
+    @extend %width-content;
+    @extend %flex-column;
+    align-content: flex-start;
+    gap: 48px;
+
+    @include mobile {
+      align-content: center;
+
+      gap: 16px;
+    }
+  }
+
+  &__line {
+    @extend %width-main;
+    height: 1px;
+
+    background-color: $color-main;
+
+    @include mobile {
+      display: none;
+    }
+  }
+
+  &__title {
+    @include fontUnify(42, 50, 700);
+    text-transform: uppercase;
+
+    @include mobile {
+      @include fontUnify(20, 30);
+      text-align: center;
+    }
+  }
+
+  &__info {
+    @include fontUnify(20, 28);
+    letter-spacing: 0.02em;
+
+    @include mobile {
+      @include fontUnify;
+
+      margin-bottom: 24px;
+    }
+  }
+  &__info-text {
+    margin-bottom: 16px;
+
+    @include mobile {
+      margin-bottom: 4px;
+    }
+  }
+
+  &__info-list {
+    list-style: disc;
+
+    padding-left: 18px;
+  }
+
+  &__goal {
+    @include mobile {
+      margin-bottom: 24px;
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+
+  &__goal-title {
+    @include fontUnify(32, 42, 600);
+    letter-spacing: 0.02em;
+
+    margin-bottom: 24px;
+
+    @include mobile {
+      @include fontUnify(20, 28);
+      text-align: center;
+
+      margin-bottom: 16px;
+    }
+  }
+
+  &__goal-text {
+    @include fontUnify(20, 28);
+    letter-spacing: 0.02em;
+
+    @include mobile {
+      @include fontUnify;
+    }
   }
 }
 </style>
