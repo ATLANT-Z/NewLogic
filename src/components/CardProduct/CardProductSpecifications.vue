b9bf57a1141e64156ce0a0d253c2014e7fa27eb3
Fatal1990 <96417741+Fatal1990@users.noreply.github.com>
2022-09-23 16:26:14 +0300

    card_instruction

--- src/components/CardProduct/CardProductSpecifications.vue
+++ src/components/CardProduct/CardProductSpecifications.vue
@@ -7,20 +7,16 @@
       </div>
       <section class="spec__description">
         <div class="ui-anchor" id="description"></div>
-        <h3 class="spec__description-title">
+        <h3 class="spec__title">
           Описание
-          <span class="spec__article"
-            >UPS 500VA + литиевая (LiFePO4) батарея 750W</span
-          >
+          <span class="spec__article">UPS 500VA + литиевая (LiFePO4) батарея 750W</span>
         </h3>
         <div class="ui-rich-text" v-html="contentFromServer"></div>
       </section>
       <section class="spec__characteristics">
-        <h3 class="spec__characteristics-title">
+        <h3 class="spec__title">
           Характеристики
-          <span class="spec__characteristics-article"
-            >GV-074-IP-H-COА14-20 3МР (Lite)</span
-          >
+          <span class="spec__article">GV-074-IP-H-COА14-20 3МР (Lite)</span>
         </h3>
         <div class="spec__characteristics-cont">
           <div class="spec__characteristics-name">Вес, кг:</div>
@@ -34,15 +30,62 @@
           </div>
         </div>
       </section>
+      <section class="instruction">
+        <h3 class="spec__title">
+          Загрузки
+          <span class="spec__article">UPS 500VA + литиевая (LiFePO4) батарея 750W</span>
+        </h3>
+        <div class="instruction__title-w">
+          <span class="instruction__title">Тип</span>
+          <span class="instruction__title">Дата</span>
+          <span class="instruction__title">Язык</span>
+          <span class="instruction__title">Тип файла</span>
+          <span class="instruction__title">Загрузка</span>
+        </div>
+        <div class="instruction__tech-w">
+          <div class="instruction__tech-title-w">
+            <span class="instruction__tech-title">Техническая документация</span>
+          </div>
+          <div class="instruction__tech-row" v-for="(item, idx) of techData" :key="idx">
+            <span class="instruction__tech-row-title">{{item.title}}</span>
+            <span class="instruction__tech-row-date">{{item.date}}</span>
+            <span class="instruction__tech-row-lang">{{item.lang}}</span>
+            <span class="instruction__tech-row-type">{{item.type}}</span>
+            <a class="instruction__tech-row-link" :href="item.link">Скачать</a>
+          </div>
+        </div>
+        <div class="instruction__tech-w">
+          <div class="instruction__tech-title-w">
+            <span class="instruction__tech-title">Драйвера</span>
+          </div>
+          <div class="instruction__tech-row" v-for="(item, idx) of techData" :key="idx">
+            <span class="instruction__tech-row-title">{{item.title}}</span>
+            <span class="instruction__tech-row-date">{{item.date}}</span>
+            <span class="instruction__tech-row-lang">{{item.lang}}</span>
+            <span class="instruction__tech-row-type">{{item.type}}</span>
+            <a class="instruction__tech-row-link" :href="item.link">Скачать</a>
+          </div>
+        </div>
+        <div class="instruction__tech-w">
+          <div class="instruction__tech-title-w">
+            <span class="instruction__tech-title">Мобильные приложения</span>
+          </div>
+          <div class="instruction__tech-row" v-for="(item, idx) of techData" :key="idx">
+            <span class="instruction__tech-row-title">{{item.title}}</span>
+            <span class="instruction__tech-row-date">{{item.date}}</span>
+            <span class="instruction__tech-row-lang">{{item.lang}}</span>
+            <span class="instruction__tech-row-type">{{item.type}}</span>
+            <a class="instruction__tech-row-link" :href="item.link">Скачать</a>
+          </div>
+        </div>
+        <div class="instruction__tech-w"></div>
+        <div class="instruction__tech-w"></div>
+      </section>
     </div>
     <div class="product-card__position">
       <div class="product-card__w">
         <div class="product-card__img-w">
-          <img
-            class="product-card__img"
-            src="@/assets/img/sliderPromImg.png"
-            alt=""
-          />
+          <img class="product-card__img" src="@/assets/img/sliderPromImg.png" alt="" />
         </div>
         <div class="product-card__money" @click="isSale = !isSale">
           <div class="product-card__money-sale" v-if="isSale">
@@ -67,6 +110,23 @@ import { Options, Vue } from "vue-class-component";
 export default class CardProductSpecificationsComponent extends Vue {
   isSale: boolean = true;
   contentFromServer: string = "";
+
+  techData: any[] = [
+    {
+      title: "Руководство пользователя",
+      date: "23.01.2020",
+      lang: "укр; рус;",
+      type: "pdf",
+      link: "https://greenvision.ua/download.php?url=https://logicfox.info/manuals/LP/UPS/16157/16157_16158_161159_17014_17015_manual_ukr.pdf"
+    },
+    {
+      title: "Руководство пользователя IP камеры",
+      date: "27.05.2021",
+      lang: "eng",
+      type: "pdf",
+      link: "https://greenvision.ua/download.php?url=https://logicfox.info/manuals/LP/UPS/16157/16157_16158_161159_17014_17015_manual_ukr.pdf"
+    }
+  ]
 }
 </script>
 
@@ -86,8 +146,19 @@ export default class CardProductSpecificationsComponent extends Vue {
     gap: 16px;
   }
 
-  &__buywith-title {
+  &__title {
+    @include fontUnify(18, 22, 700);
+
+    @include bigMobile {
+      font-weight: 500;
+    }
   }
+
+  &__article {
+    font-weight: 400;
+  }
+
+  &__buywith-title {}
 
   &__description {
     @extend %flex-column;
@@ -97,51 +168,25 @@ export default class CardProductSpecificationsComponent extends Vue {
     border-radius: 8px;
 
     padding: var(--local-pad);
-  }
 
-  &__description-title {
-    @include fontUnify(18, 22, 700);
+    & .spec__title {
+      margin-bottom: 24px;
 
-    margin-bottom: 24px;
-
-    @include bigMobile {
-      font-weight: 500;
+      @include bigMobile {
+        margin-bottom: 16px;
+      }
     }
-
-    @include bigMobile {
-      margin-bottom: 16px;
-    }
-  }
-
-  &__article {
-    font-weight: 400;
   }
 
   &__characteristics {
     @extend %flex-column;
-    gap: 24px;
+    gap: 16px;
 
     background-color: white;
     box-shadow: 0px 3px 11px rgba(0, 0, 0, 0.2);
     border-radius: 8px;
 
     padding: var(--local-pad);
-
-    @include mobile {
-      gap: 16px;
-    }
-  }
-
-  &__characteristics-title {
-    @include fontUnify(18, 22, 700);
-
-    @include bigMobile {
-      font-weight: 500;
-    }
-  }
-
-  &__characteristics-article {
-    font-weight: 400;
   }
 
   &__characteristics-cont {
@@ -168,6 +213,25 @@ export default class CardProductSpecificationsComponent extends Vue {
     @include bigMobile {
       @include fontUnify(14, 20);
     }
+  }
+}
+
+.instruction {
+  @extend %flex-column;
+  gap: 16px;
+
+  background-color: white;
+  box-shadow: 0px 3px 11px rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
+
+  padding: var(--local-pad) 0;
+
+  @include mobile {
+    gap: 16px;
+  }
+
+  & .spec__title {
+    padding: 0 var(--local-pad);
   }
 }
 
@@ -199,8 +263,7 @@ export default class CardProductSpecificationsComponent extends Vue {
     width: 100%;
   }
 
-  &__img {
-  }
+  &__img {}
 
   &__money {
     width: 100%;
